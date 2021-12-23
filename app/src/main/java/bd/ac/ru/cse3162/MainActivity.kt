5a88604b9a7db863697ff926e5e5985d8ae137f9
Shadman-Rana <shadmanrana.cseru@gmail.com>
2021-12-23 15:29:37 +0600

    modify

--- app/src/main/java/bd/ac/ru/cse3162/MainActivity.kt
+++ app/src/main/java/bd/ac/ru/cse3162/MainActivity.kt
@@ -2,11 +2,29 @@ package bd.ac.ru.cse3162
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.location.FusedLocationProviderClient
+
+import com.google.android.gms.location.LocationServices
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        bSearch.setOnClickListener {
+            getLastLocation()
+        }
     }
+
+    private fun getLastLocation() {
+        TODO("Not yet implemented")
+    }
+
+
 }
